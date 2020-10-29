f59cc94fe6bd7739da0677b10ec5ae3ace778456
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-10-29 14:49:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,21 +4,19 @@ $username ="217138";
 $password ="07052002As!a";
 $dbname ="joannastanik_bazadanych";
 
-$conn = new mysqli("$servername, $username, $password, $dbname");
+$conn = new mysqli($servername, $username, $password, $dbname);
 $sql = "SELECT * FROM pracownicy";
-$wynik = mysqli_query($conn, $sql);
+$result = mysqli_query($conn, $sql);
 
-
-    echo('<table border="1">');
+echo('<table border="1">');
     echo('<th>Imie</th><th>zarobki</th>');
 
-    while($wiersz=mysqli_fetch_assoc($wynik)){
+    while($row=mysqli_fetch_assoc($result)){
         echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>');
         echo('</tr>');
     }
 
     echo('</table>');
-
 
 ?>
