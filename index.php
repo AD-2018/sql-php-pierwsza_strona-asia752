1ea2ab4ae65bb316137771a46c7f49d5e2e49f98
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-10-29 14:46:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
-echo("<h3>Witaj swiecie</h3><br>");
-echo("Jestem Joanna Stanik");
+$servername ="mysql-joannastanik.alwaysdata.net";
+$username ="217138";
+$password ="07052002As!a";
+$dbname ="joannastanik_bazadanych";
+
+$conn = new mysqli("$servername, $username, $password, $dbname");
+$sql = "SELECT * FROM pracownicy";
+$wynik = mysqli_query($conn, $sql);
+
+
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik)){
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+
+
 ?>
