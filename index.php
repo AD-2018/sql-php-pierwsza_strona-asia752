69b33fc7657ddc80b3da1f5c7f62c28f24dc7442
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-10-30 12:09:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
-$servername ="mysql-joannastanik.alwaysdata.net";
-$username ="217138";
-$password ="blkn7!J";
-$dbname ="joannastanik_bazadanych";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+require_once("lib.php");
 $sql = "SELECT * FROM pracownicy";
 $result = mysqli_query($conn, $sql);
 
