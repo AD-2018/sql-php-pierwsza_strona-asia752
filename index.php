f163721d30bb6afca468b24dbbfc5b17fa07ecaa
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-05 14:40:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body>
   <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-asia752">Github</a>
   <div="nav">
-    <a href="orgPacownicy.php">orgPracownicy</a>
+    <a href="orgPracownicy.php">orgPracownicy</a>
     <a href="funcAgregujace.php">funcAgregujace</a>
   </div>  
   <h1>Joanna Stanik nr 12</h1>
