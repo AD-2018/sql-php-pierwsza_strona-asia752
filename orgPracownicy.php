0233d06770a0c0b48cb77e745566d0ecfeed1a5c
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-05 14:51:20 +0100

    Update orgPracownicy.php

--- orgPracownicy.php
+++ orgPracownicy.php
@@ -1,1 +1,11 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+    <div="nav">
+      <a href="index.php">funcAgregujace</a>
+      <a href="orgPracownicy.php">orgPracownicy</a>
+  </div> 
+</head>
+<body>
+<?php
