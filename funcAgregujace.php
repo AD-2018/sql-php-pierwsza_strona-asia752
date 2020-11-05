b8b8bc3bde47d8ca2939619ec5474f953062b801
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-05 14:50:47 +0100

    Update funcAgregujace.php

--- funcAgregujace.php
+++ funcAgregujace.php
@@ -2,6 +2,10 @@
 <html>
 <head>
 <link rel="stylesheet" href="style.css">
+    <div="nav">
+      <a href="index.php">funcAgregujace</a>
+      <a href="orgPracownicy.php">orgPracownicy</a>
+  </div> 
 </head>
 <body>
 <?php
