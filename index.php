dc0a669bb57518a4b3f9818732ac9f06cb4f5eff
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-05 13:57:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -29,6 +29,52 @@ echo('<table border="1">');
     }
 
     echo('</table>');
+  
+  echo ("<br>Zad.2<br>");
+$sql = "SELECT * FROM pracownicy WHERE imie like '%a'";
+echo ($sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+  
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+  
+  echo ("<br>Zad.3<br>");
+$sql = "SELECT * FROM pracownicy WHERE imie not like '%a'";
+echo ($sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+  
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+
+
 
 ?>
 </body>
