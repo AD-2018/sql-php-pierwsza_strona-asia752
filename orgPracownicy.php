d89957147ba9e7a76c9ed23e9c6a397907e44fb6
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-06 12:05:07 +0100

    Update orgPracownicy.php

--- orgPracownicy.php
+++ orgPracownicy.php
@@ -32,6 +32,72 @@ echo('<table border="1">');
     }
 
     echo('</table>');
+    
+echo ("<br>Zad.2<br>");
+$sql = "SELECT imie, nazwa_dzial FROM pracownicy, organizacja where id_org=dzial";
+echo ("<li>".$sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+  
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>dzial</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['nazwa_dzial'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+    
+echo ("<br>Zad.3<br>");
+$sql = "SELECT imie, nazwa_dzial FROM pracownicy, organizacja where id_org=dzial and imie like '%a'";
+echo ("<li>".$sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+  
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>dzial</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['nazwa_dzial'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+    
+    echo ("<br>Zad.4<br>");
+$sql = "SELECT imie, nazwa_dzial FROM pracownicy, organizacja where id_org=dzial and imie not like '%a'";
+echo ("<li>".$sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+  
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>dzial</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['nazwa_dzial'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
 ?>
 </body>
 </html>
