3e9b6e59d6592d3ded92ecec1ad0d9899af4a49f
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-06 11:49:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,66 @@
   <h1>Joanna Stanik nr 12</h1>
 <?php
 require_once("lib.php");
+  
+  echo ("<br>Zad.1<br>");
+$sql = "SELECT * FROM pracownicy WHERE dzial=2";
+echo ("<li>".$sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+  
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th><th>dzial</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>'.'<td>'.$row['dzial'].'</td>');
+        echo('</tr>');
+    }
+ 
+echo ("<br>Zad.2<br>");
+$sql = "SELECT * FROM pracownicy WHERE dzial=2 and dzial=3";
+echo ("<li>".$sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+  
+
+echo('<table border="1">');
+echo('<th>Imie</th><th>zarobki</th><th>dzial</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>'.'<td>'.$row['dzial'].'</td>');
+        echo('</tr>');
+    }
+  
+ echo ("<br>Zad.3<br>");
+$sql = "SELECT * FROM pracownicy WHERE zarobki<30";
+echo ("<li>".$sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+  
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>');
+        echo('</tr>');
+    }
 
 ?>
 </body>
