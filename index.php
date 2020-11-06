d17cdb68f6cbf81045c6db7bc7da13b6c369c674
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-06 11:51:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,26 +12,6 @@
   <h1>Joanna Stanik nr 12</h1>
 <?php
 require_once("lib.php");
-  
-  echo ("<br>Zad.1<br>");
-$sql = "SELECT * FROM pracownicy WHERE dzial=2";
-echo ("<li>".$sql);
-  $result = mysqli_query($conn, $sql);
-    if ( $result) {
-        echo "<li>ok";
-    } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-  
-
-echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th><th>dzial</th>');
-
-    while($row=mysqli_fetch_assoc($result)){
-        echo('<tr>');
-        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>'.'<td>'.$row['dzial'].'</td>');
-        echo('</tr>');
-    }
  
 echo ("<br>Zad.2<br>");
 $sql = "SELECT * FROM pracownicy WHERE dzial=2 and dzial=3";
