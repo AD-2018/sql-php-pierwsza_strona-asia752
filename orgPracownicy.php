cdd723ff725145230b9b9589061b21f8dcefa413
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-06 11:41:32 +0100

    Update orgPracownicy.php

--- orgPracownicy.php
+++ orgPracownicy.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <link rel="stylesheet" href="style.css">
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-asia752">Github</a>
     <div="nav">
       <a href="index.php">menu</a>
       <a href="funcAgregujace.php">funcAgregujace</a>
