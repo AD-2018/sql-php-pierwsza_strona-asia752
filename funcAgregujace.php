05feb020166189e87e26334f60f12950170319c3
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-12 22:44:38 +0100

    Update funcAgregujace.php

--- funcAgregujace.php
+++ funcAgregujace.php
@@ -2,7 +2,6 @@
 <html>
 <head>
 <link rel="stylesheet" href="style.css">
-  <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-asia752">Github</a>
   <nav class="nav">
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-asia752">Github</a>
     <a class="link" href="index.php">Strona Główna</a>
