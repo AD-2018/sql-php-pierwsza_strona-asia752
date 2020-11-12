0adb2a795b9393eed7aecd7c1710c5e5d624c0c6
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-12 22:16:39 +0100

    Update dataczas.php

--- dataczas.php
+++ dataczas.php
@@ -191,6 +191,71 @@ echo('<table border="1">');
 
 echo('</table>');
 
+echo("<br>Zad 9<br>");
+$sql = "select nazwa_dzial, max(YEAR(CURDATE())-YEAR(data_urodzenia)) as Wiek from pracownicy, organizacja where id_org=dzial group by dzial";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Dział</th><th>Wiek</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['nazwa_dzial'].'</td><td>'.$row['Wiek'].'</td>');
+        echo('</tr>');
+    }
+
+echo('</table>');
+  
+echo("<br>Zad 10<br>");
+$sql = "select nazwa_dzial, min(YEAR(CURDATE())-YEAR(data_urodzenia)) as Wiek from pracownicy, organizacja where id_org=dzial and nazwa_dzial='handel' or nazwa_dzial='serwis group by dzial";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Dział</th><th>Wiek</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['nazwa_dzial'].'</td><td>'.$row['Wiek'].'</td>');
+        echo('</tr>');
+    }
+
+echo('</table>');
+  
+echo("<br>Zad 11<br>");
+$sql = "select imie, nazwa_dzial, max(YEAR(CURDATE())-YEAR(data_urodzenia)) as Wiek from pracownicy, organizacja where id_org=dzial and nazwa_dzial='handel' or nazwa_dzial='serwis' group by dzial";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Dział</th><th>Wiek</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['nazwa_dzial'].'</td><td>'.$row['Wiek'].'</td>');
+        echo('</tr>');
+    }
+
+echo('</table>');
 ?>
 </body>
 </html>
