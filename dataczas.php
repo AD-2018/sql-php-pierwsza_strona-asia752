888ba13fb288e85274bffdc3e163227734ecfc29
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-12 14:32:27 +0100

    Update dataczas.php

--- dataczas.php
+++ dataczas.php
@@ -12,3 +12,30 @@
   <h1>Joanna Stanik nr 12</h1>
 </head>
 <body>
+<?php
+require_once("lib.php");
+  
+echo ("<br>Zad.1<br>");
+$sql = "SELECT imie, data_urodzenia, year(curdate())-year(data_urodzenia) as wiek from pracownicy";
+echo ("<li>".$sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+  
+
+echo('<table border="1">');
+    echo('<th>imie</th><th>data urodzenia</th><th>wiek</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['data_urodzenia'].'</td>'.'<td>'.$row['wiek'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+  ?>
+  </body>
+</html>
