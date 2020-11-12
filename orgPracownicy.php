bef890e7c0883a6336265ee813c08d2e49fb1253
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-12 13:42:28 +0100

    Update orgPracownicy.php

--- orgPracownicy.php
+++ orgPracownicy.php
@@ -235,6 +235,28 @@ echo('<table border="1">');
     }
 
     echo('</table>');
+  
+  echo ("<br>Zad.11<br>");
+$sql = "SELECT imie, nazwa_dzial, zarobki FROM pracownicy, organizacja WHERE id_org=dzial and imie like '%a' and (dzial=4 or dzial=2) order by zarobki desc limit 2";
+echo ("<li>".$sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+  
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>dzial</th><th>zarobki</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['nazwa_dzial'].'</td>'.'<td>'.$row['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
 ?>
 </body>
 </html>
