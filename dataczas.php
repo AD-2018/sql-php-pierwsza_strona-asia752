befdaa46f3b4ad6d327c0eae4eca2bd65a579cd7
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-12 22:45:06 +0100

    Update dataczas.php

--- dataczas.php
+++ dataczas.php
@@ -3,7 +3,7 @@
 <head>
 <link rel="stylesheet" href="style.css">
   <nav class="nav">
-    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-asia752">Github</a>
+    <a class="link" href="https://github.com/AD-2018/sql-php-pierwsza_strona-asia752">Github</a>
     <a class="link" href="index.php">Strona Główna</a>
     <a class="link" href="orgPracownicy.php">Pracownicy i organizacja</a>
     <a class="link" href="funcAgregujace.php">Funkcje Agregujące</a>
