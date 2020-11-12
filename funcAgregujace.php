f9f73ec46e2c8eed8263c2f9ec25efb61c865d8f
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-12 14:02:11 +0100

    Update funcAgregujace.php

--- funcAgregujace.php
+++ funcAgregujace.php
@@ -232,6 +232,25 @@ echo('<table border="1">');
         echo('</tr>');
     }
   echo('</table>');
+  
+    echo ("<br>Zad.4<br>");
+$sql = "SELECT sum(zarobki) as suma, if( (imie LIKE '%a'), 'Kobiety','Mężczyźni') as 'plec' FROM `pracownicy` GROUP by plec";
+echo ("<li>".$sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+echo('<table border="1">');
+    echo('<th>plec</th><th>Suma</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['plec'].'</td>'.'<td>'.$row['suma'].'</td>');
+        echo('</tr>');
+    }
+  echo('</table>');
 
 ?>
 </body>
