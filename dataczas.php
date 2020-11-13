f08c4bcbe726c0e61bb47908f909fc219682e279
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-13 11:22:33 +0100

    Update dataczas.php

--- dataczas.php
+++ dataczas.php
@@ -515,7 +515,7 @@ echo('<table border="1">');
 echo('</table>');
   
 echo("<br>Zad 10<br>");
-$sql = "SELECT DATE_FORMAT(data_urodzenia,'%W') as dzien, count(date_format(data_urodzenia, '%W')) as Liczba FROM pracownicy where ORDER BY CASE
+$sql = "SELECT DATE_FORMAT(data_urodzenia,'%W') as dzien, count(date_format(data_urodzenia, '%W')) as Liczba FROM pracownicy ORDER BY CASE
           
           WHEN dzien = 'Poniedziałek' THEN 1
           WHEN dzien = 'Wtorek' THEN 2
