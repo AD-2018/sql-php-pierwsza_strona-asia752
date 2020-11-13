27cdbbed20a7fadd540ddf71454e5c477033182f
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-13 10:27:17 +0100

    Update dataczas.php

--- dataczas.php
+++ dataczas.php
@@ -394,6 +394,28 @@ echo('<table border="1">');
     }
 
 echo('</table>');
+  
+echo("<br>Zad 5<br>");
+$sql = "select imie, date_format(data_urodzenia, '%Y-%M-%W') as data from pracownicy";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>Data Urodzenia</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td><td>'.$row['data'].'</td>');
+        echo('</tr>');
+    }
+
+echo('</table>');
 ?>
 </body>
 </html>
