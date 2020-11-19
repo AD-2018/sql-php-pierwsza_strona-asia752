bb43519cdfab317628bf0b44b29626faaa4f61c5
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-19 14:40:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     <a class="link" href="dataczas.php">Data i czas</a>
     <a class="link" href="formularz.html">Formularz</a>
     <a class="link" href="insert.php">Insert</a>
+    <a class="link" href="daneDoBazy.php">Dane do bazy</a>
   </nav> 
   <h1>Joanna Stanik nr 12</h1>
 <?php
