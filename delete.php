70d0bb0ff7990aeded4b85817e682238fd13eaf3
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-11-26 13:36:38 +0100

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,27 @@
+<?php
+echo("jestes w delete.php <br>");
+echo $_POST['id_pracownicy'];
+
+
+$servername ="mysql-joannastanik.alwaysdata.net";
+$username ="217138";
+$password ="blkn7!J";
+$dbname ="joannastanik_bazadanych";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "DELETE FROM Pracownik WHERE id=".$_POST['id_pracownicy'];
+
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
