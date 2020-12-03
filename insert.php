38845d8734d7324942aff3c22ca7fda6da9b9472
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-12-03 13:16:45 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -5,15 +5,7 @@ echo "<li>".$_POST['dzial'];
 echo "<li>".$_POST['zarobki'];
 echo "<li>".$_POST['data_urodzenia'];
 
-$servername ="mysql-joannastanik.alwaysdata.net";
-$username ="217138";
-$password ="blkn7!J";
-$dbname ="joannastanik_bazadanych";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+require_once("lib.php");
 
 $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
        VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
