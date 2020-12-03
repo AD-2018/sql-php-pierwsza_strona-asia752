0d480e545f46fb316094b564064e14c885939c7d
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-12-03 13:21:58 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -12,6 +12,7 @@ $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodze
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
+  header('Location: https://stanikjoanna.herokuapp.com/daneDoBazy.php');
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
