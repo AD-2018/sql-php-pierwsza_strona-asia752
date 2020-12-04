2be61fa0ab265cd3d6224dd274aa8d6f76498916
Joanna Stanik <63036694+JoannaStanik@users.noreply.github.com>
2020-12-04 11:58:07 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -37,6 +37,50 @@ echo('<table border="1">');
     }
 
     echo('</table>');
+  
+  echo ("<br>Zad.1<br>");
+$sql = "SELECT * FROM bibl_autor";
+echo ("<li>".$sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+  
+
+echo('<table border="2">');
+    echo('<th>Id</th><th>Tytuł</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['id_tytul'].'</td><td>'.$row['tytul'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+  
+  echo ("<br>Zad.3<br>");
+$sql = "SELECT * FROM bibl_autor";
+echo ("<li>".$sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+  
+
+echo('<table border="1">');
+    echo('<th>Id</th><th>Id Autor</th><th>Id tytuł</th><wypoz</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['id_book'].'</td><td>'.$row['id_autor'].'</td><td>'.$row['id_tytul'].'</td><td>'.$row['wypoz'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
     ?>
     </body>
     </html>
