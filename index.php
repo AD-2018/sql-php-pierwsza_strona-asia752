f41127490f5e86fabe38536b5d7cddde75d62c1c
JoannaStanik <asia.stanik752@gmail.com>
2021-02-25 15:46:21 +0100

    p

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <body>
   <div class="container">
     <div class="tytul">
-      <a class="link" href="https://github.com/AD-2018/sql-php-pierwsza_strona-asia752">Github</a>
+      <a class="github" href="https://github.com/AD-2018/sql-php-pierwsza_strona-asia752">Github</a>
       <div class="imie">
       <h1>Joanna Stanik 3Ti nr 12</h1>
     </div>
